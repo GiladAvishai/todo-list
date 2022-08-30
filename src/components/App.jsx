455607c2a036722315acc9559ff2a16a3a40979b
Gilad Avishai <giladavishai42@gmail.com>
2022-08-30 21:59:58 +0300

    added ul to demonstrate the list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,5 +2,11 @@ import React from "react";
 import InputArea from "./InputArea";
 
 export default function App() {
-    return <div><InputArea/></div>
+    return (
+    <div>
+        <InputArea/>
+        <ul>
+            <li>Buy Milk</li>
+        </ul>
+    </div>);
 }
