a023795f7c79a15f613ccdba4824fb4f22866aa6
Gilad Avishai <giladavishai42@gmail.com>
2022-08-30 22:03:34 +0300

    added comments to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import InputArea from "./InputArea";
 
+// add todoItem component and add to app
+// add header and footer
+// add logic to todo-list
+// style the app
+
 export default function App() {
     return (
     <div>
