2ad6c8b6f52d52ae83648eb0f6747a0d7a7c8cf5
Gilad Avishai <giladavishai42@gmail.com>
2022-09-17 18:07:31 +0300

    added style and icon to the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import LoupeIcon from '@mui/icons-material/Loupe';
 
 export default function Header() {
     return (
         <header>
-            <h1>Todo-List</h1>
+            <h1><LoupeIcon/>Todo</h1>
         </header>
     );
 }
