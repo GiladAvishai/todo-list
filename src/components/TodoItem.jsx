64f23f1f67294882c5b513063c22bacc494bee1b
Gilad Avishai <giladavishai42@gmail.com>
2022-09-17 17:15:21 +0300

    added delete button without functionality to TodoItem.jsx

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 
 export default function TodoItem(props) {
-    return (
+  return (
+    <div>
+      <li>
+        {props.itemName}
         <div>
-            <li>{props.itemName}</li>
+          <button>
+            <DeleteOutlineIcon></DeleteOutlineIcon>
+          </button>
         </div>
-    );
+      </li>
+    </div>
+  );
 }
