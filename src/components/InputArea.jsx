c5909aa67bdd8f1016e09a2e01e5e35345c733f9
Gilad Avishai <giladavishai42@gmail.com>
2022-09-30 00:24:38 +0300

    added style to input area

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -1,6 +1,19 @@
 import React, { useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import Fab from "@mui/material/Fab";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Zoom from "@mui/material/Zoom";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#AAC4FF",
+    },
+    secondary: {
+      main: "#11cb5f",
+    },
+  },
+});
 
 export default function InputArea(props) {
   const [inputText, setInputText] = useState("");
@@ -11,17 +24,23 @@ export default function InputArea(props) {
   }
 
   return (
-    <div>
+    <div id="inputarea" theme={theme}>
       <h1 id="listheader">My List</h1>
       <input value={inputText} onChange={handleChange} />
-      <Fab
-        onClick={() => {
-          props.onAdd(inputText);
-          setInputText("");
-        }}
-      >
-        <AddIcon />
-      </Fab>
+      <ThemeProvider theme={theme}>
+        <Zoom in={true}>
+          <Fab
+            size="small"
+            color="primary"
+            onClick={() => {
+              props.onAdd(inputText);
+              setInputText("");
+            }}
+          >
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </ThemeProvider>
     </div>
   );
 }
